ed686a15bbdde84a015351a3c16a4eb0bce031fd
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-07-29 23:14:49 +0200

    render: check ray intersection

--- Src/draw/render.c
+++ Src/draw/render.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fbelotti <marvin@42perpignan.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 23:40:28 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/07/29 03:00:02 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/07/29 23:12:58 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/miniRT.h"
+
+static void	check_ray_intersection(t_object *object, t_data *data, int x, int y)
+{
+	if (object->type == SPHERE)
+	{
+		if (sphere_intersection(data, &object->specific.sphere,
+			&data->rays[y * WINDOW_WIDTH + x].direction) == 1)
+	}
+	else if (object->type == CYLINDER)
+	{
+		if (cylinder_intersection(data, &object->specific.cylinder,
+			&data->rays[y * WINDOW_WIDTH + x].direction) == 1)
+			ft_mlx_pixel_put(data->img, x, y);
+	}
+	else if (object->type == PLANE)
+	{
+		if (plane_intersection(data, &object->specific.plane,
+			&data->rays[y * WINDOW_WIDTH + x].direction) == 1)
+			ft_mlx_pixel_put(data->img, x, y);
+	}
+}
 
 void	render(t_data *data)
 {
@@ -28,21 +49,7 @@ void	render(t_data *data)
 			current_object = data->objects;
 			while (current_object)
 			{
-				if (current_object->type == SPHERE)
-				{
-					if (sphere_intersection(data, &current_object->specific.sphere, &data->rays[y * WINDOW_WIDTH + x].direction) == 1)
-						ft_mlx_pixel_put(data->img, x, y);
-				}
-				else if (current_object->type == CYLINDER)
-				{
-					if (cylinder_intersection(data, &current_object->specific.cylinder, &data->rays[y * WINDOW_WIDTH + x].direction) == 1)
-						ft_mlx_pixel_put(data->img, x, y);
-				}
-				else if (current_object->type == PLANE)
-				{
-					if (plane_intersection(data, &current_object->specific.plane, &data->rays[y * WINDOW_WIDTH + x].direction) == 1)
-						ft_mlx_pixel_put(data->img, x, y);
-				}
+				check_ray_intersection(current_object, data, x, y);
 				current_object = current_object->next;
 			}
 			x++;
