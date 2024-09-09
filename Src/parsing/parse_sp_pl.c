26b7b7a21489121fda1c18672f5123f7114c8cb2
Beorlor <jbdurand2000@gmail.com>
2024-09-09 17:55:27 +0200

    plane leak

--- Src/parsing/parse_sp_pl.c
+++ Src/parsing/parse_sp_pl.c
@@ -6,7 +6,7 @@
 /*   By: jedurand <jedurand@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:24:28 by jedurand          #+#    #+#             */
-/*   Updated: 2024/09/09 16:24:29 by jedurand         ###   ########.fr       */
+/*   Updated: 2024/09/09 17:54:59 by jedurand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,18 @@ int	parse_plane_data(char **split, t_object *obj)
 	pos_split = ft_split(split[1], ',');
 	normal_split = ft_split(split[2], ',');
 	color_split = ft_split(split[3], ',');
-	if (!pos_split || !normal_split || !color_split)
-		return (free_split(pos_split), free_split(pos_split),
-			free_split(normal_split), free_split(color_split),
-			ft_printf("Error: Invalid plane format\n"), 1);
+    // Check if any of the splits failed or if they contain invalid data
+    if (!pos_split || !pos_split[0] || !pos_split[1] || !pos_split[2] ||
+        !normal_split || !normal_split[0] || !normal_split[1] || !normal_split[2] ||
+        !color_split || !color_split[0] || !color_split[1] || !color_split[2])
+    {
+        // Free any allocated resources before returning the error
+        free_split(pos_split);
+        free_split(normal_split);
+        free_split(color_split);
+        ft_printf("Error: Invalid plane format\n");
+        return (1);
+    }
 	obj->pos.x = ft_atof(pos_split[0]);
 	obj->pos.y = ft_atof(pos_split[1]);
 	obj->pos.z = ft_atof(pos_split[2]);
@@ -101,7 +109,7 @@ int	parse_plane(char **split, t_data *data)
 	}
 	ft_memset(obj, 0, sizeof(t_object));
 	if (parse_plane_data(split, obj))
-		return (1);
+		return (free(obj), 1);
 	obj->type = PLANE;
 	obj->checkerboard = false;
 	ft_memset(&obj->noise, 0, sizeof(t_noise));
